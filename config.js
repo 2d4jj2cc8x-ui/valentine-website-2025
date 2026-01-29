680f3a0afaaaec9f7fe753505a1bfc2fbf3480b0
2d4jj2cc8x-ui <2d4jj2cc8x@privaterelay.appleid.com>
2026-01-29 11:58:59 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Marcoz",
+    valentineName: "Antonica <3",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
@@ -33,7 +33,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you marry me? 🌹", // The big question!
+            text: "Will you be my Valentine?? 🌹", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
